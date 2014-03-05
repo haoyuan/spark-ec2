f8df094644b91a197ec9e7d7ab2d699a9b2b64cf
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-03-05 14:49:57 -0800

    add mvn and compile tachyon master branch

--- tachyon/init.sh
+++ tachyon/init.sh
@@ -7,14 +7,31 @@ if [ -d "tachyon" ]; then
   return 0
 fi
 
-TACHYON_VERSION=0.4.1
+# TACHYON_VERSION=0.4.1
+TACHYON_VERSION=9.9.9
 
 # Github tag:
-if [[ "$TACHYON_VERSION" == *\|* ]]
+if [[ "$TACHYON_VERSION" == 9.9.9 ]]
 then
-  # Not yet supported
-  echo ""
-# Pre-package tachyon version
+  # Setup Maven
+
+  wget http://apache.claz.org/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz ;
+  tar -xzvf apache-maven-3.0.5-bin.tar.gz ;
+  mv apache-maven-3.0.5 maven ;
+  rm -rf apache-maven-3.0.5-bin.tar.gz ;
+  echo "export M2_HOME=/root/maven" >> .bashrc ;
+  echo "export M2=\$M2_HOME/bin" >> .bashrc ;
+  echo "export MAVEN_OPTS=\"-Xms256m -Xmx512m\"" >> .bashrc ;
+  echo "export PATH=\$M2:\$PATH" >> .bashrc ;
+  echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> .bashrc ;
+  source .bashrc;
+
+  # Setup Tachyon
+  echo "Get lastest Tachyon from github:"
+  git clone git@github.com:amplab/tachyon.git
+  cd tachyon
+  mvn clean package
+# Pre-package Tachyon version
 else
   case "$TACHYON_VERSION" in
     0.3.0)
